b5a487ad6170fff12b97c1956959903c34033d3e
Emery Haller <Emery.E.Haller-1@ou.edu>
2021-08-25 16:06:42 -0500

    final change

--- ConsoleApp1/Loops Part 2/Program.cs
+++ ConsoleApp1/Loops Part 2/Program.cs
@@ -13,7 +13,7 @@ namespace Loops_Part_2
             {
                 examCount++;
                 Console.WriteLine($"Please enter your exam score for Exam  {examCount.ToString("N0")} (example 90% = .9)");
-                sum = Convert.ToDouble(Console.ReadLine());
+                sum += Convert.ToDouble(Console.ReadLine());
 
                 Console.WriteLine("Do you have another score to enter? Yes or No");
             } while(Console.ReadLine().ToLower()[0] == 'y');
