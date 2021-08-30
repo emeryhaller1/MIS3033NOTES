5e9752da6ba1557c30c98d8f5d666c69ee3efbfc
Emery Haller <Emery.E.Haller-1@ou.edu>
2021-08-30 16:06:51 -0500

    changes

---
+++ ConsoleApp1/Arrays1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Arrays1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] fruits = new string[5];
+            fruits[0] = "Apples";
+            fruits[1] = "Oranges";
+            fruits[2] = "Bananas";
+            fruits[3] = "Grapes";
+            fruits[4] = "Blueberries";
+
+            double[] prices = { 0.99, 0.50, 0.50, 2.99, 1.99 };
+
+            Console.WriteLine("What fruit would you like the price for?");
+            string fruit = Console.ReadLine().ToLower();
+
+            int fruitIndex = -1; //indexes start at 0 and so we do -1 so that it is never that
+
+
+            for (int i = 0; i < fruits.Length; i++)
+            {
+                if (fruits[i] == fruit)
+                {
+                    fruitIndex = i;
+                    break;
+                }
+            }
+
+            if (fruitIndex >= 0)
+            {
+                Console.WriteLine($"The price of {fruit}'s is {prices[fruitIndex].ToString("C")}");
+            }
+            else
+            {
+                Console.WriteLine($"We do not carry {fruit} here. Go elsewhere please.");
+            }
+        }
+    }
+}
+
---
+++ ConsoleApp1/Arrays2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Arrays2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] lowercaseLetters = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+            char[] uppercaseLetters = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+
+            string firstName = uppercaseLetters[4].ToString() + lowercaseLetters[12].ToString() + lowercaseLetters[4].ToString() + lowercaseLetters[17].ToString() + lowercaseLetters[24].ToString();
+            Console.WriteLine(firstName);
+        }
+    }
+}
