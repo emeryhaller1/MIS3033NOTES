cc390b0580584237b70ee87012b0ba5c0d94e31a
Emery Haller <Emery.E.Haller-1@ou.edu>
2021-08-30 16:12:35 -0500

    changes

---
+++ ConsoleApp1/Dictionaries/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dictionaries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, double> fruits = new Dictionary<string, double>();
+
+            fruits.Add("Apples", 0.99);
+            fruits.Add("Oranges", 0.50);
+            fruits.Add("Bananas", 0.50);
+            fruits.Add("Grapes", 2.99);
+            fruits.Add("Blueberries", 1.99);
+
+            Console.WriteLine("What fruit would you like the price for?");
+            String fruit = Console.ReadLine();
+
+            if (fruits.ContainsKey(fruit) == true)
+            {
+                Console.WriteLine($"The price of {fruit}'s is {fruits[fruit].ToString()}");
+
+            }
+            else
+            {
+                Console.WriteLine($"We do not carry {fruit} here. Go elsewehere please.");
+            }
+        }
+
+    }
+}
